44ff068e66302aea80ec35e5eb9f6fd4e36caf8a
jeel2308 <42006763+jeel2308@users.noreply.github.com>
2020-01-09 22:35:01 +0530

    Updates

---
+++ precache-manifest.aa4c9c0bf09637f21109fc45d1235218.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87c30f10b36d366de89ddee77c785f60",
+    "url": "/Calc/index.html"
+  },
+  {
+    "revision": "d333469aca4e214f7256",
+    "url": "/Calc/static/css/main.3e094f46.chunk.css"
+  },
+  {
+    "revision": "4db1eed491f750d56de5",
+    "url": "/Calc/static/js/2.5e86b069.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Calc/static/js/2.5e86b069.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d333469aca4e214f7256",
+    "url": "/Calc/static/js/main.54934a3e.chunk.js"
+  },
+  {
+    "revision": "d9122d842f7fbda6fd5e",
+    "url": "/Calc/static/js/runtime-main.d14b6d80.js"
+  }
+]);
